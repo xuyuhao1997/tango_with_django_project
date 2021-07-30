a29e5dd6ec81e955f4919a027d5b935ce8de8d6d
xuyuhao1997 <2511671X@student.gla.ac.uk>
2021-07-30 20:50:03 +0100

    chapter 4 commit

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from rango import views
+from django.shortcuts import render
+from django.http import HttpResponse
 
 app_name = 'rango'
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-]
+
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+     return render(request, 'rango/about.html')
